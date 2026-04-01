dfd3e17cf967d61a0c5bd13819b974292947d33f
ikennaiwu <piusiwu1@gmail.com>
2026-04-01 07:33:33 +0200

    fix: update wildcard route and CORS for production

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use("/api/categories", categoryRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
+	app.get("/{*splat}", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
